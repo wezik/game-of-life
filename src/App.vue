ad8111068c8b3d356aafc0edd4969d0bb2f8dd17
Dominik Kondrot <dominik.kondrot@gmail.com>
2023-11-21 23:16:05 +0100

    Refactor refactored base implementation

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,187 @@
 <script setup>
-import TheGame from './components/TheGame.vue'
+
+import { ref } from 'vue';
+
+const height = 20;
+const width = 20;
+const msDelay = 200;
+
+const state = ref(getInitialState());
+
+const runButtonStartMessage = "START";
+const runButtonStopMessage = "STOP";
+const runButton = ref({
+  disabled: false,
+  message: runButtonStartMessage
+});
+
+const cycleButtonMessage = "CYCLE";
+const cycleButton = ref({
+  disabled: false,
+  message: cycleButtonMessage
+});
+
+const restartButtonMessage = "RESTART";
+const restartButton = ref({
+  disabled: false,
+  message: restartButtonMessage
+});
+
+function getInitialState() {
+  return {
+    isRunning: false,
+    board: createEmptyBoard() 
+  }
+}
+
+function createEmptyBoard() {
+  let board = [];
+  for (let y = 0; y < height; y++) {
+    let row = [];
+    for (let x = 0; x < width; x++) {
+      let cell = createCell(y * width + x, false);
+      row.push(cell);
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+function createCell(id, isAlive) {
+  return {
+    id: id,
+    isAlive: isAlive
+  };
+}
+
+function restartState() {
+  if (state.value.isRunning) {
+    changeState();
+  }
+  state.value.board = createEmptyBoard();
+}
+
+let interval = null;
+
+function changeState() {
+  state.value.isRunning = !state.value.isRunning;
+  let currentState = state.value.isRunning;
+  interval = currentState ? setInterval(runCycle, msDelay) : clearInterval(interval);
+  
+  runButton.value.message = currentState ? runButtonStopMessage : runButtonStartMessage;
+  cycleButton.value.disabled = currentState;
+}
+
+function runCycle() {
+  let board = state.value.board;
+  let newBoard = [];
+  for (let y = 0; y < board.length; y++) {
+    let newRow = [];
+    for (let x = 0; x < board[y].length; x++) {
+      let cell = createCell(board[y][x].id, calculateLife(x, y));
+      newRow[x] = cell;
+    }
+    newBoard.push(newRow);
+  }
+  state.value.board = newBoard;
+}
+
+function calculateLife(x, y) {
+  let board = state.value.board;
+  let sum = countNeighbours(x, y);
+
+  let stayAliveCondition = sum == 2 && board[y][x].isAlive;
+  let bornCondition = sum == 3;
+
+  return stayAliveCondition || bornCondition;
+}
+
+function countNeighbours(x, y) {
+  let sum = 0;
+  for (let yOffset = -1; yOffset < 2; yOffset++) {
+    for (let xOffset = -1; xOffset < 2; xOffset++) {
+      if (xOffset != 0 || yOffset != 0) {
+        sum += checkCellIsAlive(x + xOffset, y + yOffset) ? 1 : 0;
+      }
+    }
+  }
+  return sum;
+}
+
+function checkCellIsAlive(x, y) {
+  let board = state.value.board;
+
+  let yIsOutOfBounds = y < 0 || y >= board.length;
+  if (yIsOutOfBounds) return false;
+
+  let xIsOutOfBounds = x < 0 || x >= board[y].length;
+  if (xIsOutOfBounds) return false;
+
+  return board[y][x].isAlive;
+}
+
 </script>
 
 <template>
-
   <header>
+    <button class="btn" @click="restartState" :disabled="restartButton.disabled">{{ restartButton.message }}</button>
+    <button class="btn" @click="changeState" :disabled="runButton.disabled">{{ runButton.message }}</button>
+    <button class="btn" @click="runCycle" :disabled="cycleButton.disabled">{{ cycleButton.message }}</button>
   </header>
-
   <main>
-    <TheGame />
+    <div class="board">
+      <div class="row" v-for="row in state.board">
+        <div class="cell" v-for="cell in row" @click="cell.isAlive = !cell.isAlive" :class="{ 'cell--alive': cell.isAlive }">
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
   header {
+    padding-top: 2em;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
+    gap: 5px;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  main {
+    margin-top: 2em;
+    display: flex;
+    justify-content: center;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .btn {
+    height: 2em;
+    font-weight: 600;
   }
-}
+  
+  .board {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .cell {
+    height: 20px;
+    width: 20px;
+    background-color: rgb(50, 50, 50);
+    transition: .15s;
+    border-radius: 5px;
+    border: 1px solid black;
+  }
+
+  .cell--alive {
+    background-color: white;
+  }
+
+  .cell:hover {
+    cursor: pointer;
+  }
+
 </style>
